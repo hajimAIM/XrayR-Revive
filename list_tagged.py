8513868614c889c107795f0d7681ebd8fa0b4f51
hajimAIM <hajimxmiku12@gmail.com>
2025-12-09 17:14:02 +0800

    chore: remove temporary debug scripts

--- list_tagged.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = r"C:\Users\user\go\pkg\mod\github.com\xtls\xray-core@v1.251208.0\transport\internet\tagged"
-try:
-    for f in os.listdir(path):
-        print(f)
-except Exception as e:
-    print(e)
--- read_tagged.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = r"C:\Users\user\go\pkg\mod\github.com\xtls\xray-core@v1.251208.0\transport\internet\tagged\tagged.go"
-try:
-    with open(path, 'r', encoding='utf-8') as f:
-        print(f.read())
-except Exception as e:
-    print(e)
